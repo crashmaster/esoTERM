14d0ca972b7005c91e54603a6ffe3cb990a14617
Johnny Crash <cannon.imus@gmail.com>
2014-06-04 16:13:25 +0200

    New feature requests :)

--- pinfo.lua
+++ pinfo.lua
@@ -1,17 +1,19 @@
 --
--- Player Info Layout:
+-- Planned player info layout:
 --
--- [#1] [#2], [#3] [#4], [#5] [#6], [#7] [#8]
+-- [#1] [#2], [#3] [#4], [#5] [#6] [#7], [#8] [#9] [#10]
 --
 -- Legend
---  #1 - Alliance vs. Alliance (AvA.) Rank (e.g.: Sergeant)
---  #2 - Character Name (e.g.: Hank)
---  #3 - Level Information (e.g.: Veteran 3)
---  #4 - Class (e.g.: Dragon Knight)
---  #5 - Actual Level Experience / Maximal Level Experience (e.g.: 123 / 500)
---  #6 - Last Experience Gain (e.g.: +72)
---  #7 - Actual AvA. Experience / Maximal AvA Experience (e.g.: 321 / 800)
---  #8 - Last AvA Experience Gain (e.g.: +8)
+--  #1  - Alliance vs. Alliance (AvA.) Rank (e.g.: Sergeant)
+--  #2  - Character Name (e.g.: Hank)
+--  #3  - Level Information (e.g.: Veteran 3)
+--  #4  - Class (e.g.: Dragon Knight)
+--  #5  - Actual Level Experience / Maximal Level Experience (e.g.: 123/500)
+--  #6  - Actual Level Experience per cent (e.g.: 24,6%)
+--  #7  - Last Experience Gain (e.g.: +72)
+--  #8  - Actual AvA. Experience / Maximal AvA Experience (e.g.: 321/800)
+--  #9  - Last AvA. Experience Gain (e.g.: +8)
+--  #10 - Actual AvA. Experience per cent (e.g.: 44,6%)
 --
 -- Not applicable information is skipped.
 --
