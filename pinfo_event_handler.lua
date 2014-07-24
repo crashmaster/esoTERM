b43158e05e43a89f6eb6198c093a6172131f7832
Johnny Crash <cannon.imus@gmail.com>
2014-07-24 13:24:43 +0200

    Added level up event handling.
    Fixed some stuff in xp event handling, some tests are needed.

--- pinfo_event_handler.lua
+++ pinfo_event_handler.lua
@@ -4,22 +4,42 @@ pinfo_event_handler = {}
 
 function pinfo_event_handler.initialize()
     EVENT_MANAGER:RegisterForEvent(pinfo.ADDON_NAME,
+                                   EVENT_EXPERIENCE_UPDATE,
+                                   pinfo_event_handler.on_experience_update)
+
+    EVENT_MANAGER:RegisterForEvent(pinfo.ADDON_NAME,
                                    EVENT_VETERAN_POINTS_UPDATE,
                                    pinfo_event_handler.on_experience_update)
 
     EVENT_MANAGER:RegisterForEvent(pinfo.ADDON_NAME,
-                                   EVENT_EXPERIENCE_UPDATE,
-                                   pinfo_event_handler.on_experience_update)
+                                   EVENT_LEVEL_UPDATE,
+                                   pinfo_event_handler.on_level_update)
+
+    EVENT_MANAGER:RegisterForEvent(pinfo.ADDON_NAME,
+                                   EVENT_VETERAN_RANK_UPDATE,
+                                   pinfo_event_handler.on_level_update)
 end
 
 function pinfo_event_handler.on_experience_update(event, unit, xp, xp_max, reason)
-    if ((unit == "player") and (reason > -1) and (xp_max ~= 0)) then
-        CACHE.xp_gain = xp - CACHE.level_xp
+    if ((unit == "player") and (xp_max ~= 0) and (CACHE.level_xp ~= xp)) then
+        if (reason > -1) then
+            CACHE.xp_gain = xp - CACHE.level_xp
+        end
         CACHE.level_xp = xp
         CACHE.level_xp_max = xp_max
-        CACHE.level_xp_percent = xp * 100 / xp_max
+        if (xp > xp_max) then
+            CACHE.level_xp_percent = 100
+        else
+            CACHE.level_xp_percent = xp * 100 / xp_max
+        end
 
         pinfo_output.character_info_to_debug()
+    end
+end
+
+function pinfo_event_handler.on_level_update(event, unit, level)
+    if (unit == "player") then
+        CACHE.level = level
     end
 end
 
--- test_pinfo_event_handler.lua
+++ test_pinfo_event_handler.lua
@@ -37,6 +37,16 @@ describe("Test event handler initialization", function()
             event = EVENT_VETERAN_POINTS_UPDATE,
             callback = pinfo_event_handler.on_experience_update
         }
+        expected_register_params.level_update = {
+            addon_name = pinfo.ADDON_NAME,
+            event = EVENT_LEVEL_UPDATE,
+            callback = pinfo_event_handler.on_level_update
+        }
+        expected_register_params.veteran_rank_update = {
+            addon_name = pinfo.ADDON_NAME,
+            event = EVENT_VETERAN_RANK_UPDATE,
+            callback = pinfo_event_handler.on_level_update
+        }
     end
 
     local function when_initialize_is_called_with()
@@ -68,70 +78,17 @@ describe("Test event handler initialization", function()
     end)
 end)
 
-describe("Test the on experience update event handler", function()
-    local cache = pinfo.CHARACTER_INFO
-
-    local EVENT = -1
+describe("Test event handlers", function()
+    local EVENT = nil
     local UNIT = "player"
-    local REASON = 0
-
-    local OLD_XP = 100
-    local OLD_XP_MAX = 1000
-    local OLD_XP_PCT = OLD_XP * 100 / OLD_XP_MAX
-    local OLD_XP_GAIN = 0
-    local NEW_XP = 200
-    local NEW_XP_MAX = 2000
-    local NEW_XP_PCT = NEW_XP * 100 / NEW_XP_MAX
-
-    before_each(function()
-        cache.level_xp = OLD_XP
-        cache.level_xp_max = OLD_XP_MAX
-        cache.level_xp_percent = OLD_XP_PCT
-        cache.xp_gain = OLD_XP_GAIN
-        pinfo_event_handler.initialize()
-    end)
-
-    after_each(function()
-        cache = {}
-        ut_helper.restore_stubbed_functions()
-    end)
 
     -- {{{
     local function given_that_pinfo_output_character_info_to_debug_is_stubbed()
         ut_helper.stub_function(pinfo_output, "character_info_to_debug", nil)
     end
 
-    local function when_on_experience_update_is_called_with(event, unit, xp, xp_max, reason)
-        pinfo_event_handler.on_experience_update(event, unit, xp, xp_max, reason)
-    end
-
-    local function then_the_xp_properties_in_character_info_where_updated()
-        assert.is.equal(NEW_XP, cache.level_xp)
-        assert.is.equal(NEW_XP_MAX, cache.level_xp_max)
-        assert.is.equal(NEW_XP_PCT, cache.level_xp_percent)
-        assert.is.equal(NEW_XP - OLD_XP, cache.xp_gain)
-    end
-
     local function and_pinfo_output_character_info_to_debug_was_called_once()
         assert.spy(pinfo_output.character_info_to_debug).was.called_with()
-    end
-    -- }}}
-
-    it("On experience update, happy flow",
-    function()
-        given_that_pinfo_output_character_info_to_debug_is_stubbed()
-
-        when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, NEW_XP_MAX, REASON)
-
-        then_the_xp_properties_in_character_info_where_updated()
-            and_pinfo_output_character_info_to_debug_was_called_once()
-    end)
-
-    -- {{{
-    local function then_the_xp_properties_in_character_info_where_not_updated()
-        assert.is.equal(OLD_XP, cache.level_xp)
-        assert.is.equal(OLD_XP_MAX, cache.level_xp_max)
-        assert.is.equal(OLD_XP_PCT, cache.level_xp_percent)
     end
 
     local function and_pinfo_output_character_info_to_debug_was_not_called()
@@ -139,34 +96,136 @@ describe("Test the on experience update event handler", function()
     end
     -- }}}
 
-    it("On experience update, incorrect unit",
-    function()
-        given_that_pinfo_output_character_info_to_debug_is_stubbed()
+    describe("Test the on experience update event handler", function()
+        local cache = pinfo.CHARACTER_INFO
 
-        when_on_experience_update_is_called_with(EVENT, "foo", NEW_XP, NEW_XP_MAX, REASON)
+        local REASON = 0
+        local OLD_XP = 100
+        local OLD_XP_MAX = 1000
+        local OLD_XP_PCT = OLD_XP * 100 / OLD_XP_MAX
+        local OLD_XP_GAIN = 0
+        local NEW_XP = 200
+        local NEW_XP_MAX = 2000
+        local NEW_XP_PCT = NEW_XP * 100 / NEW_XP_MAX
 
-        then_the_xp_properties_in_character_info_where_not_updated()
-            and_pinfo_output_character_info_to_debug_was_not_called()
+        before_each(function()
+            cache.level_xp = OLD_XP
+            cache.level_xp_max = OLD_XP_MAX
+            cache.level_xp_percent = OLD_XP_PCT
+            cache.xp_gain = OLD_XP_GAIN
+        end)
+
+        after_each(function()
+            cache = {}
+            ut_helper.restore_stubbed_functions()
+        end)
+
+        -- {{{
+        local function when_on_experience_update_is_called_with(event, unit, xp, xp_max, reason)
+            pinfo_event_handler.on_experience_update(event, unit, xp, xp_max, reason)
+        end
+
+        local function then_the_xp_properties_in_character_info_where_updated()
+            assert.is.equal(NEW_XP, cache.level_xp)
+            assert.is.equal(NEW_XP_MAX, cache.level_xp_max)
+            assert.is.equal(NEW_XP_PCT, cache.level_xp_percent)
+            assert.is.equal(NEW_XP - OLD_XP, cache.xp_gain)
+        end
+        -- }}}
+
+        it("On experience update, happy flow",
+        function()
+            given_that_pinfo_output_character_info_to_debug_is_stubbed()
+
+            when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, NEW_XP_MAX, REASON)
+
+            then_the_xp_properties_in_character_info_where_updated()
+                and_pinfo_output_character_info_to_debug_was_called_once()
+        end)
+
+        -- {{{
+        local function then_the_xp_properties_in_character_info_where_not_updated()
+            assert.is.equal(OLD_XP, cache.level_xp)
+            assert.is.equal(OLD_XP_MAX, cache.level_xp_max)
+            assert.is.equal(OLD_XP_PCT, cache.level_xp_percent)
+        end
+        -- }}}
+
+        it("On experience update, incorrect unit",
+        function()
+            given_that_pinfo_output_character_info_to_debug_is_stubbed()
+
+            when_on_experience_update_is_called_with(EVENT, "foo", NEW_XP, NEW_XP_MAX, REASON)
+
+            then_the_xp_properties_in_character_info_where_not_updated()
+                and_pinfo_output_character_info_to_debug_was_not_called()
+        end)
+
+        it("On experience update, incorrect reason",
+        function()
+            given_that_pinfo_output_character_info_to_debug_is_stubbed()
+
+            when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, NEW_XP_MAX, -1)
+
+            then_the_xp_properties_in_character_info_where_not_updated()
+                and_pinfo_output_character_info_to_debug_was_not_called()
+        end)
+
+        it("On experience update, total maximum xp reached",
+        function()
+            given_that_pinfo_output_character_info_to_debug_is_stubbed()
+
+            when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, 0, REASON)
+
+            then_the_xp_properties_in_character_info_where_not_updated()
+                and_pinfo_output_character_info_to_debug_was_not_called()
+        end)
     end)
 
-    it("On experience update, incorrect reason",
-    function()
-        given_that_pinfo_output_character_info_to_debug_is_stubbed()
+    describe("Test the on level update event handler", function()
+        local cache = pinfo.CHARACTER_INFO
 
-        when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, NEW_XP_MAX, -1)
+        local OLD_LEVEL = 1
+        local NEW_LEVEL = 2
 
-        then_the_xp_properties_in_character_info_where_not_updated()
-            and_pinfo_output_character_info_to_debug_was_not_called()
-    end)
+        before_each(function()
+            cache.level = OLD_LEVEL
+        end)
 
-    it("On experience update, total maximum xp reached",
-    function()
-        given_that_pinfo_output_character_info_to_debug_is_stubbed()
+        after_each(function()
+            cache = {}
+            ut_helper.restore_stubbed_functions()
+        end)
 
-        when_on_experience_update_is_called_with(EVENT, UNIT, NEW_XP, 0, REASON)
+        -- {{{
+        local function when_on_level_update_is_called_with(event, unit, level)
+            pinfo_event_handler.on_level_update(event, unit, level)
+        end
 
-        then_the_xp_properties_in_character_info_where_not_updated()
-            and_pinfo_output_character_info_to_debug_was_not_called()
+        local function then_the_level_property_in_character_info_was_updated()
+            assert.is.equal(NEW_LEVEL, cache.level)
+        end
+        -- }}}
+
+        it("On level update, happy flow",
+        function()
+            when_on_level_update_is_called_with(EVENT, UNIT, NEW_LEVEL)
+
+            then_the_level_property_in_character_info_was_updated()
+        end)
+
+        -- {{{
+        local function then_the_level_property_in_character_info_was_not_updated()
+            assert.is.equal(OLD_LEVEL, cache.level)
+        end
+        -- }}}
+
+        it("On level update, incorrect unit",
+        function()
+            when_on_level_update_is_called_with(EVENT, "foo", NEW_LEVEL)
+
+            then_the_level_property_in_character_info_was_not_updated()
+        end)
     end)
 end)
 
