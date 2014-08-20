3ab42fa04b71a3f1e838abf381b0dfe81d7a9d74
Johnny Crash <cannon.imus@gmail.com>
2014-08-20 18:36:41 +0200

    Message buffering, when chat it not ready.
    There are situation (when starting a new character), where
    xp is gained before printing to chat is available.
    
    TODO: remove buffer content, when finished processing it.

--- pinfo_output.lua
+++ pinfo_output.lua
@@ -4,6 +4,8 @@ pinfo_output = {}
 pinfo_output.PROMPT = "[pinfo] "
 pinfo_output.default_settings = {chat_tab_number = 1}
 
+local xp_message_buffer = {}
+local ap_message_buffer = {}
 
 function pinfo_output.initialize()
     pinfo_output.settings = ZO_SavedVars:New("pinfo_saved_variables",
@@ -27,6 +29,49 @@ function pinfo_output.set_n_th_chat_tab_as_output(chat_tab_number)
     pinfo_output.system_message("Output chat tab is: " .. pinfo_output.chat_tab_name)
 end
 
+local function xp_message_to_print()
+    return string.format("+%s+ +%d+ +%s+ +%.2f%%+ (+%d XP)",
+                pinfo_char.get_character_name(CACHE),
+                pinfo_char.get_character_level(CACHE),
+                pinfo_char.get_character_class(CACHE),
+                pinfo_char.get_character_level_xp_percent(CACHE),
+                pinfo_char.get_character_xp_gain(CACHE))
+end
+
+local function ap_message_to_print()
+    return string.format("+%s+ +%s+ +%s+ +%.2f%%+ (+%d AP)",
+                pinfo_char.get_character_ava_rank_name(CACHE),
+                pinfo_char.get_character_name(CACHE),
+                pinfo_char.get_character_class(CACHE),
+                pinfo_char.get_character_ava_rank_points_percent(CACHE),
+                pinfo_char.get_character_ava_points_gain(CACHE))
+end
+
+local function store_xp_message_before_player_activated()
+    table.insert(xp_message_buffer, xp_message_to_print())
+end
+
+local function print_xp_message()
+    pinfo_output.chat_tab:AddMessage(xp_message_to_print())
+end
+
+local function store_ap_message_before_player_activated()
+    table.insert(ap_message_buffer, ap_message_to_print())
+end
+
+local function print_ap_message()
+    pinfo_output.chat_tab:AddMessage(ap_message_to_print())
+end
+
+pinfo_output.xp_to_chat_tab = store_xp_message_before_player_activated
+pinfo_output.ap_to_chat_tab = store_ap_message_before_player_activated
+
+local function print_message_buffer(buffer)
+    for _, message in ipairs(buffer) do
+        pinfo_output.chat_tab:AddMessage(message)
+    end
+end
+
 function pinfo_output.on_player_activated(event)
     local chat_tab_number = pinfo_output.default_settings.chat_tab_number
 
@@ -35,26 +80,12 @@ function pinfo_output.on_player_activated(event)
     end
 
     pinfo_output.set_n_th_chat_tab_as_output(chat_tab_number)
-end
 
-function pinfo_output.xp_to_chat_tab()
-    pinfo_output.chat_tab:AddMessage(
-        string.format("+%s+ +%d+ +%s+ +%.2f%%+ (+%d XP)",
-            pinfo_char.get_character_name(CACHE),
-            pinfo_char.get_character_level(CACHE),
-            pinfo_char.get_character_class(CACHE),
-            pinfo_char.get_character_level_xp_percent(CACHE),
-            pinfo_char.get_character_xp_gain(CACHE)))
-end
+    print_message_buffer(xp_message_buffer)
+    pinfo_output.xp_to_chat_tab = print_xp_message
 
-function pinfo_output.ap_to_chat_tab()
-    pinfo_output.chat_tab:AddMessage(
-        string.format("+%s+ +%s+ +%s+ +%.2f%%+ (+%d AP)",
-            pinfo_char.get_character_ava_rank_name(CACHE),
-            pinfo_char.get_character_name(CACHE),
-            pinfo_char.get_character_class(CACHE),
-            pinfo_char.get_character_ava_rank_points_percent(CACHE),
-            pinfo_char.get_character_ava_points_gain(CACHE)))
+    print_message_buffer(ap_message_buffer)
+    pinfo_output.ap_to_chat_tab = print_ap_message
 end
 
 function pinfo_output.system_message(message)
