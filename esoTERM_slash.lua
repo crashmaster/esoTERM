a40e286f6895515d691339debe5eca73b1472efd
Johnny Crash <cannon.imus@gmail.com>
2015-01-04 11:19:34 +0100

    Update slash commands module

--- esoTERM_slash.lua
+++ esoTERM_slash.lua
@@ -1,20 +1,39 @@
 esoTERM_slash = {}
 
+local function get_module_status(module)
+    return string.format("%s  <%s>",
+                         module.module_name,
+                         module.is_active and "ACTIVE" or "INACTIVE")
+end
+
+local function get_module_statuses(module_register)
+    local array = {}
+    for i, module in ipairs(module_register) do
+        table.insert(array, get_module_status(module))
+    end
+    return array
+end
+
 function esoTERM_slash.slash_command_handler(command)
-    if command == "" then
-        esoTERM_output.sysout("Running")
-    elseif string.lower(command) == "help" then
-        esoTERM_output.sysout("No help here")
-    elseif string.lower(command) == "status" then
-        for i, module in ipairs(esoTERM.module_register) do
-            local m = string.format("%s  <%s>",
-                                    module.module_name,
-                                    module.is_active and "ACTIVE" or "INACTIVE")
-            esoTERM_output.sysout(m)
+    local output
+    local lower_case_command = string.lower(command)
+    if lower_case_command == "" then
+        output = esoTERM.ADDON_NAME .. " active"
+    elseif lower_case_command == "help" then
+        output = "No help here"
+    elseif lower_case_command == "status" then
+        local register = esoTERM.module_register
+        if #register == 0 then
+            output = "No registered modules"
+        elseif #register == 1 then
+            output = get_module_status(register[1])
+        else
+            output = table.concat(get_module_statuses(register), "\n")
         end
     else
-        esoTERM_output.sysout("Invalid command: " .. command)
+        output = "Invalid command: " .. command
     end
+    esoTERM_output.sysout(output)
 end
 
 function esoTERM_slash.initialize()
--- tests/test_esoTERM_slash.lua
+++ tests/test_esoTERM_slash.lua
@@ -19,12 +19,22 @@ describe("Test slash command handler initialization.", function()
 end)
 
 describe("Test slash command handlers.", function()
+    local fake_active_module = {
+        module_name = "fake_active_module",
+        is_active = true
+    }
+    local fake_inactive_module = {
+        module_name = "fake_inactive_module",
+        is_active = false
+    }
+
     before_each(function()
         ut_helper.stub_function(esoTERM_output, "sysout", nil)
     end)
 
     after_each(function()
         ut_helper.restore_stubbed_functions()
+        esoTERM.module_register = {}
     end)
 
     -- {{{
@@ -37,27 +47,100 @@ describe("Test slash command handlers.", function()
     end
     -- }}}
 
-    it("No command provided", function()
+    it("Output of no command", function()
         when_command_handler_called_with("")
 
-        then_sysout_was_called_with("Running")
+        then_sysout_was_called_with("esoTERM active")
     end)
 
-    it("Invalid command provided", function()
+    it("Output of invalid command", function()
         when_command_handler_called_with("foobar")
 
         then_sysout_was_called_with("Invalid command: foobar")
     end)
 
-    it("Lower case help provided", function()
+    it("Output of lower case help command", function()
         when_command_handler_called_with("help")
 
         then_sysout_was_called_with("No help here")
     end)
 
-    it("Mixed case help provided", function()
+    it("Output of mixed case help command", function()
         when_command_handler_called_with("hElP")
 
         then_sysout_was_called_with("No help here")
+    end)
+
+    -- {{{
+    local function given_that_there_is_no_module_registered()
+        esoTERM.module_register = {}
+    end
+    -- }}}
+
+    it("Output of status command if no module registered", function()
+        given_that_there_is_no_module_registered()
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("No registered modules")
+    end)
+
+    -- {{{
+    local function given_that_there_is_one_module_registered(module)
+        esoTERM_common.register_module(esoTERM.module_register, module)
+    end
+    -- }}}
+
+    it("The status command returns the status of one active module", function()
+        given_that_there_is_one_module_registered(fake_active_module)
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("fake_active_module  <ACTIVE>")
+    end)
+
+    it("The status command returns the status of one inactive module", function()
+        given_that_there_is_one_module_registered(fake_inactive_module)
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("fake_inactive_module  <INACTIVE>")
+    end)
+
+    -- {{{
+    local function given_that_there_are_two_module_registered(module_1, module_2)
+        esoTERM_common.register_module(esoTERM.module_register, module_1)
+        esoTERM_common.register_module(esoTERM.module_register, module_2)
+    end
+    -- }}}
+
+    it("The status command returns the status of two active modules", function()
+        given_that_there_are_two_module_registered(fake_active_module,
+                                                   fake_active_module)
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("fake_active_module  <ACTIVE>\n" ..
+                                    "fake_active_module  <ACTIVE>")
+    end)
+
+    it("The status command returns the status of two inactive modules", function()
+        given_that_there_are_two_module_registered(fake_inactive_module,
+                                                   fake_inactive_module)
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("fake_inactive_module  <INACTIVE>\n" ..
+                                    "fake_inactive_module  <INACTIVE>")
+    end)
+
+    it("The status command returns the status of an active and an inactive module", function()
+        given_that_there_are_two_module_registered(fake_active_module,
+                                                   fake_inactive_module)
+
+        when_command_handler_called_with("status")
+
+        then_sysout_was_called_with("fake_active_module  <ACTIVE>\n" ..
+                                    "fake_inactive_module  <INACTIVE>")
     end)
 end)
