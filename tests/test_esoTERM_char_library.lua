7c6ec3d2b7e8af5d7ded7b4358791558a424df44
Johnny Crash <cannon.imus@gmail.com>
2015-01-10 12:47:31 +0100

    Add test libraries to reduce redundancy

---
+++ tests/test_esoTERM_char_library.lua
@@ -0,0 +1,21 @@
+local test_library = require("test_library")
+
+test_esoTERM_char_library = {}
+
+GLOBAL = _G
+
+PLAYER = "player"
+
+GENDER_1 = test_library.A_STRING
+GENDER_2 = test_library.B_STRING
+CLASS_1 = test_library.A_STRING
+CLASS_2 = test_library.B_STRING
+NAME_1 = test_library.A_STRING
+NAME_2 = test_library.B_STRING
+COMBAT_STATE_1 = test_library.A_BOOL
+COMBAT_STATE_2 = test_library.B_BOOL
+COMBAT_START_TIME = test_library.A_INTEGER
+COMBAT_LENGHT = test_library.A_INTEGER
+COMBAT_DAMAGE = test_library.A_INTEGER
+
+return test_esoTERM_char_library
---
+++ tests/test_library.lua
@@ -0,0 +1,10 @@
+test_library = {}
+
+test_library.A_BOOL = true
+test_library.B_BOOL = false
+test_library.A_INTEGER = 1111
+test_library.B_INTEGER = 2222
+test_library.A_STRING = "aAaAa"
+test_library.B_STRING = "bBbBb"
+
+return test_library
