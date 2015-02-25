262ac3e723f43390ab1f783a6631dac44348ced2
Johnny Crash <cannon.imus@gmail.com>
2015-02-25 21:36:00 +0100

    Update X

--- tests/test_esoTERM_char_library.lua
+++ tests/test_esoTERM_char_library.lua
@@ -39,22 +39,24 @@ this.EXPECTED_CACHED_VALUES = {
     combat_damage = this.COMBAT_DAMAGE
 }
 
+-- register_for_event {{{
 this.EXPECTED_REGISTER_FOR_EVENT_CALLS = {}
 
 function this.expected_register_for_event_calls_are_cleared()
     this.EXPECTED_REGISTER_FOR_EVENT_CALLS = nil
 end
 
-function this.and_that_getter_functions_are_stubbed()
-    for getter, return_value in pairs(this.RETURN_VALUES_OF_THE_GETTER_STUBS) do
-        test_library.stub_function_with_return_value(esoTERM_char, getter, return_value)
-    end
-end
-
-function this.and_getter_function_stubs_were_called()
-    for getter, _ in pairs(this.RETURN_VALUES_OF_THE_GETTER_STUBS) do
-        assert.spy(esoTERM_char[getter]).was.called_with()
-    end
+function this.and_that_expected_register_for_event_calls_are_set_up()
+    this.EXPECTED_REGISTER_FOR_EVENT_CALLS.combat_state_update = {
+        local_register = this.EVENT_REGISTER,
+        event = EVENT_PLAYER_COMBAT_STATE,
+        callback = esoTERM_char.on_combat_state_update
+    }
+    this.EXPECTED_REGISTER_FOR_EVENT_CALLS.death_state_update = {
+        local_register = this.EVENT_REGISTER,
+        event = EVENT_UNIT_DEATH_STATE_CHANGED,
+        callback = esoTERM_char.on_unit_death_state_change
+    }
 end
 
 function this.and_that_register_for_event_is_stubbed()
@@ -71,8 +73,23 @@ function this.and_register_for_event_was_called_with_expected_parameters()
         assert.is_not.equal(nil, this.EXPECTED_REGISTER_FOR_EVENT_CALLS[param].callback)
     end
 end
+-- }}}
 
+-- getter {{{
+function this.and_that_getter_functions_are_stubbed()
+    for getter, return_value in pairs(this.RETURN_VALUES_OF_THE_GETTER_STUBS) do
+        test_library.stub_function_with_return_value(esoTERM_char, getter, return_value)
+    end
+end
 
+function this.and_getter_function_stubs_were_called()
+    for getter, _ in pairs(this.RETURN_VALUES_OF_THE_GETTER_STUBS) do
+        assert.spy(esoTERM_char[getter]).was.called_with()
+    end
+end
+-- }}}
+
+-- register module {{{
 function this.and_that_register_module_is_stubbed()
     test_library.stub_function_with_no_return_value(esoTERM_common, "register_module")
 end
@@ -80,7 +97,9 @@ end
 function this.and_register_module_was_called()
     assert.spy(esoTERM_common.register_module).was.called_with(esoTERM.module_register, esoTERM_char)
 end
+-- }}}
 
+-- unregister_from_all_events {{{
 function this.and_that_unregister_from_all_events_is_stubbed()
     test_library.stub_function_with_no_return_value(esoTERM_common, "unregister_from_all_events")
 end
@@ -88,20 +107,9 @@ end
 function this.and_unregister_from_all_events_was_called()
     assert.spy(esoTERM_common.unregister_from_all_events).was.called_with(this.EVENT_REGISTER)
 end
+-- }}}
 
-function this.and_that_expected_register_for_event_calls_are_set_up()
-    this.EXPECTED_REGISTER_FOR_EVENT_CALLS.combat_state_update = {
-        local_register = this.EVENT_REGISTER,
-        event = EVENT_PLAYER_COMBAT_STATE,
-        callback = esoTERM_char.on_combat_state_update
-    }
-    this.EXPECTED_REGISTER_FOR_EVENT_CALLS.death_state_update = {
-        local_register = this.EVENT_REGISTER,
-        event = EVENT_UNIT_DEATH_STATE_CHANGED,
-        callback = esoTERM_char.on_unit_death_state_change
-    }
-end
-
+-- cache {{{
 function this.given_that_cache_is_empty()
     assert.is.equal(0, ut_helper.table_size(this.CACHE))
 end
@@ -115,18 +123,25 @@ function this.and_cached_values_became_initialized()
         assert.is.equal(expected_value, this.CACHE[cache_attribute])
     end
 end
+-- }}}
 
+-- module_name {{{
 function this.verify_that_the_module_name_is_the_expected_one()
     assert.is.equal(MODULE_NAME, esoTERM_char.module_name)
 end
+-- }}}
 
+-- initialize {{{
 function this.when_initialize_is_called()
     esoTERM_char.initialize()
 end
+-- }}}
 
+-- deactivate {{{
 function this.when_deactivate_for_the_module_is_called()
     esoTERM_char.deactivate()
 end
+-- }}}
 
 -- esoTERM_char module activeness {{{
 local function set_the_esoTERM_char_module_to_active()
