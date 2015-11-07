d07f01f42ceb953a28c5d73ecc1d4da27212db63
Johnny Crash <cannon.imus@gmail.com>
2015-11-07 22:21:56 +0100

    Refactor

--- tests/test_esoTERM_crafting.lua
+++ tests/test_esoTERM_crafting.lua
@@ -48,16 +48,15 @@ local and_inactive_state_of_the_module_was_saved = tl.and_inactive_state_of_the_
 local and_register_for_event_was_called_with = tl.and_register_for_event_was_called_with
 local and_register_module_was_called_with = tl.and_register_module_was_called_with
 local and_that_activate_is_stubbed = tl.and_that_activate_is_stubbed
-local and_that_expected_register_for_event_calls_are_set_up = tl.and_that_expected_register_for_event_calls_are_set_up
 local and_that_register_for_event_is_stubbed = tl.and_that_register_for_event_is_stubbed
 local and_that_register_module_is_stubbed = tl.and_that_register_module_is_stubbed
 local and_that_unregister_from_all_events_is_stubbed = tl.and_that_unregister_from_all_events_is_stubbed
 local and_unregister_from_all_events_was_called_with = tl.and_unregister_from_all_events_was_called_with
-local expected_register_for_event_calls_are_cleared = tl.expected_register_for_event_calls_are_cleared
-local given_that_module_is_set_active_in_the_config_file = tl.given_that_module_is_set_active_in_the_config_file
-local given_that_module_is_set_inactive_in_the_config_file = tl.given_that_module_is_set_inactive_in_the_config_file
+local get_expected_register_for_event_calls = tl.get_expected_register_for_event_calls
 local given_that_module_is_active = tl.given_that_module_is_active
 local given_that_module_is_inactive = tl.given_that_module_is_inactive
+local given_that_module_is_set_active_in_the_config_file = tl.given_that_module_is_set_active_in_the_config_file
+local given_that_module_is_set_inactive_in_the_config_file = tl.given_that_module_is_set_inactive_in_the_config_file
 local then_activate_was_called = tl.then_activate_was_called
 local then_activate_was_not_called = tl.then_activate_was_not_called
 local then_module_became_active = tl.then_module_became_active
@@ -111,20 +110,18 @@ end)
 
 describe("Test esoTERM_crafting module activate.", function()
     after_each(function()
-        expected_register_for_event_calls_are_cleared()
         ut_helper.restore_stubbed_functions()
     end)
 
     it("Update cache and subscribe for events on activate.",
     function()
         given_that_module_is_inactive(esoTERM_crafting)
-            and_that_expected_register_for_event_calls_are_set_up()
             and_that_register_for_event_is_stubbed()
 
         when_activate_is_called()
 
         then_module_became_active(esoTERM_crafting)
-            and_register_for_event_was_called_with(EXPECTED_REGISTER_FOR_EVENT_CALLS)
+            and_register_for_event_was_called_with(get_expected_register_for_event_calls())
             and_active_state_of_the_module_was_saved()
     end)
 end)
