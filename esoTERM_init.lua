fd687d8e9ce8b3e21a506b4141f82a296a212cd8
crashmaster <crashmaster@users.noreply.github.com>
2017-06-01 21:58:10 +0200

    Comment modules crashing immediately
    Things have changes

--- esoTERM_init.lua
+++ esoTERM_init.lua
@@ -3,9 +3,9 @@ esoTERM_init = {}
 function esoTERM_init.initialize(addon_name)
     if addon_name == esoTERM.ADDON_NAME then
         esoTERM_output.initialize()
-        esoTERM_champ.initialize()
-        esoTERM_char.initialize()
-        esoTERM_pve.initialize()
+--        esoTERM_champ.initialize()
+--        esoTERM_char.initialize()
+--        esoTERM_pve.initialize()
         esoTERM_pvp.initialize()
         esoTERM_loot.initialize()
         esoTERM_slash.initialize()
